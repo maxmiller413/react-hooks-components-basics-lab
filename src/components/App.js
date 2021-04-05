8f919b93c4b68c4d36312510948c65600cf99d7e
Max Miller <maxmiller413@gmail.com>
2021-04-05 17:16:10 -0500

    commit tree 691267ab38b913411106801107309bb70d27faaf

--- src/components/App.js
+++ src/components/App.js
@@ -16,10 +16,20 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div >
+      <div id="about">About</div>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <About />
       {/* add the <Home> component here */}
       {/* write an <About> component here */}
     </div>
